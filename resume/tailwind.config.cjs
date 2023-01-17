8559543e2572d7fb37f69d02312d4d2e558039ec
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-18 00:09:50 +0300

    bg added

--- resume/tailwind.config.cjs
+++ resume/tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: "Pacifico, sans-serif",
+      display: "Pacifico, sans-serif",
+    },
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('./hero-bg.svg')",
+      },
+    },
   },
   plugins: [],
 };
