26c910e6628ebbe9bdc3476825753ee44db14f63
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-19 21:38:28 +0300

    basic containers

--- resume/src/App.tsx
+++ resume/src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="w-full h-3/4 bg-hero relative">
-      <div className="flex justify-center p-16 w-full h-[34em] bg-hero-pattern bg-cover bg-left-bottom bg-no-repeat ">
-        <div className="bg-white w-full lg:max-w-5xl absolute border h-10"></div>
+    <div className="h-3/4 bg-hero relative">
+      <div className=" h-[34em] bg-hero-pattern bg-cover bg-left-bottom bg-no-repeat ">
+        <div className="mx-auto md:p-8 flex flex-col md:flex-row justify-evenly items-center max-w-7xl">
+          <div className="bg-white shrink-0 w-full h-10 md:mr-2 md:max-w-[18rem]"></div>
+          <div className="bg-white shrink w-full h-10 max-w-[50rem]"></div>
+        </div>
       </div>
     </div>
   );
