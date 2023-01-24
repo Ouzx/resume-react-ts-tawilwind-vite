61da8d1214618a75ec2c07a3a554a2f93f10c1da
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-24 21:36:32 +0300

    hello font changed

--- resume/src/components/Intro.tsx
+++ resume/src/components/Intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   const data = React.useContext(DataContext);
   return (
     <>
-      <h1 className="font-black mb-12">{data.HI}</h1>
+      <h1 className="font-thin hello mb-12">{data.HI}</h1>
       <p className="mb-14 text-darkText">{data.Description}</p>
     </>
   );
